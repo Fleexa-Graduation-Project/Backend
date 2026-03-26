382ed56e72fc4defc4937320dcf283abccab2d21
jana <janahassan2052004@gmail.com>
2026-03-26 22:52:10 +0200

    added gitignore to remove junk files

--- .history/models/mqttEnvelope_20260209034347.go
+++
@@ -1,7 +0,0 @@
-// incoming message structure
-type MQTTEnvelope struct {
-	DeviceID  string                 `json:"device_id"`
-	Timestamp int64                  `json:"timestamp"`
-	Type      string                 `json:"type"`
-	Payload   map[string]interface{} `json:"payload"`
-}
